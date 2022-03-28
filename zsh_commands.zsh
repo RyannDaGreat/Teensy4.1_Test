d41e2bd76ed6665947f77f5d052064e3fac192cb
RyanBurgert219 <sqrtryan@gmail.com>
2022-03-28 06:06:34 -0400

    Added link to buy teensy

--- zsh_commands.zsh
+++ zsh_commands.zsh
@@ -1,3 +1,4 @@
 cd /Volumes/CIRCUITPY && rm -rf * .* && git clone https://github.com/RyannDaGreat/Teensy4.1_Test . #Download repo into teensy after flashing it
 cd /Volumes/CIRCUITPY ; lazygit #Don't run lazygit while running code; it will make it refresh repeatedly
 screen  /dev/cu.usbmodem14101 ; killall screen #Run then kill when detach (via ctrl+a then ctrl+d)
+#Buy teensy here: https://protosupplies.com/product/teensy-4-1-fully-loaded/
